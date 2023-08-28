fbc8297315e876b31e4f8ba9d2df96387a0cfed1
Yunseo Kim <s2102@e-mirim.hs.kr>
2023-08-28 10:52:05 +0900

    Delegator를 활용한 구현 방법

--- DelegatePrac/Program.cs
+++ DelegatePrac/Program.cs
@@ -8,8 +8,35 @@ namespace DelegatePrac
 {
     class Program
     {
+        class Product
+        {
+            public string Name { get; set; }
+            public int Price { get; set; }
+        }
         static void Main(string[] args)
         {
+            List<Product> products = new List<Product>()
+            {
+                new Product() { Name = "감자", Price = 500 },
+                new Product() { Name = "사과", Price = 700 },
+                new Product() { Name = "고구마", Price = 400 },
+                new Product() { Name = "배추", Price = 600 },
+                new Product() { Name = "상추", Price = 300 },
+            };
+            // 정렬
+            // 1. Delegator를 활용한 구현 방법
+            products.Sort(SortWithPrice);
+
+            //출력
+            foreach (var item in products)
+            {
+                Console.WriteLine(item.Name + " : " + item.Price);
+            }
+        }
+
+        private static int SortWithPrice(Product x, Product y)
+        {
+            return x.Price.CompareTo(y.Price);
         }
     }
 }
